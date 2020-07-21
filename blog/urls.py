79fc19c271e1a547f2e181c78d3b60ffd18e269e
Gaurav Pandey <gauravguitarrocks@gmail.com>
2020-07-21 19:10:05 +0530

    views addition

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home,name='blogs-home')
+    path('',views.home,name='blogs-home'),
+    path('about/',views.about,name='blogs-about')
 ]
