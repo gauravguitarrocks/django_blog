7ba1d35d20624d642c340c99805e956ea7f3245f
Gaurav Pandey <gauravguitarrocks@gmail.com>
2020-07-28 13:16:47 +0530

    update forms added

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .form import UserRegisterForm
+from .form import UserRegisterForm,UserUpdateForm,ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -12,7 +12,7 @@ def register(request):
             user_name = form.cleaned_data.get('username')
             messages.success(
                 request, f'Account for {user_name} created succesfully')
-            return redirect('blogs-home')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
@@ -20,4 +20,26 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request,'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST,instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+
+            messages.success(
+            request, f'Profile updated successfully.')
+
+        return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+
+    return render(request,'users/profile.html',context)
